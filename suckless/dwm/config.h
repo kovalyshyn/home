88da23bef4c79bc3a3f6967e24948117fbd0d18c
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2019-02-04 12:42:21 +0200

    dwm: add stack rotate

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,5 +1,6 @@
 /* See LICENSE file for copyright and license details. */
 #include "X11/XF86keysym.h"
+#include "movestack.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -105,6 +106,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
